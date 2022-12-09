579baa312e8f2f972ee9a6ff5a91bf90936226ef
Nikita Hripach <nikita.hripach@gmail.com>
2022-12-09 13:06:19 +0300

    Basic impl of Generic Mappings

--- Task3/src/Mappings/Mappings.hs
+++ Task3/src/Mappings/Mappings.hs
@@ -1,12 +1,11 @@
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE InstanceSigs #-}
 module Mappings.Mappings
-    ( mapProductToModel
+    ( Mapping(..)
+    , mapProductToModel
     , mapOrderToModel
     , mapCustomerToModel
-    , mapShopToModel
-    , mapModelToCutomer
-    , mapModelToShop
-    , mapModelToProduct
-    , mapModelToOrder) where
+    , mapShopToModel) where
 
 import Data.Entities
     ( Product(..)
@@ -16,6 +15,87 @@ import Data.Entities
 import Data.Models (ProductModel(..), ShopModel(..), OrderModel(..), CustomerModel (..))
 import Utils.Utils (maybeHead)
 import Data.Maybe (fromMaybe)
+
+class Mapping b a where
+    toModel :: a -> b
+
+    toList :: [a] -> [b]
+    toList = map toModel
+
+instance Mapping ProductModel Product where
+    toModel :: Product -> ProductModel
+    toModel prod = ProductModel {
+        productModelId = productId prod,
+        productModelShop = Nothing,
+        productModelName = productName prod,
+        productModelPrice = productPrice prod,
+        productModelColor = productColor prod
+    }
+
+instance Mapping OrderModel Order where
+    toModel :: Order -> OrderModel
+    toModel ord = OrderModel {
+        orderModelId = orderId ord,
+        orderModelNumber = orderNumber ord,
+        orderModelCustomer = Nothing,
+        orderModelProducts = Nothing
+    }
+
+instance Mapping CustomerModel Customer where
+    toModel :: Customer -> CustomerModel
+    toModel cust = CustomerModel {
+        customerModelId = customerId cust,
+        customerModelName = customerName cust,
+        customerModelAddress = customerAddress cust,
+        customerModelOrders = Nothing
+    }
+
+instance Mapping ShopModel Shop where
+    toModel :: Shop -> ShopModel
+    toModel sp = ShopModel {
+        shopModelId = shopId sp,
+        shopModelName = shopName sp,
+        shopModelAddress = shopAddress sp,
+        shopModelProducts = Nothing
+    } 
+
+instance Mapping Product ProductModel where 
+    toModel :: ProductModel -> Product
+    toModel productModel =
+        let spId = shopModelId <$> productModelShop productModel
+        in Product {
+            productId = productModelId productModel,
+            productShopId = fromMaybe (-1) spId,
+            productName = productModelName productModel,
+            productPrice = productModelPrice productModel,
+            productColor = productModelColor productModel
+        }
+
+instance Mapping Order OrderModel where
+    toModel :: OrderModel -> Order
+    toModel orderModel =
+        let ordId = customerModelId <$> orderModelCustomer orderModel
+        in Order {
+                orderId = orderModelId orderModel,
+                orderCustomerId = fromMaybe (-1) ordId,
+                orderNumber = orderModelNumber orderModel
+        }
+
+instance Mapping Customer CustomerModel where
+    toModel :: CustomerModel -> Customer
+    toModel customerModel = Customer {
+        customerId = customerModelId customerModel,
+        customerName = customerModelName customerModel,
+        customerAddress = customerModelAddress customerModel
+    }
+
+instance Mapping Shop ShopModel where
+    toModel :: ShopModel -> Shop
+    toModel shopModel = Shop {
+        shopId = shopModelId shopModel,
+        shopName = shopModelName shopModel,
+        shopAddress = shopModelAddress shopModel
+    }
 
 mapProductToModel :: Product -> Maybe Shop -> ProductModel
 mapProductToModel prod maybeShop =
@@ -71,38 +151,4 @@ mapShopToModel sp maybeProduct =
         shopModelName = shopName sp,
         shopModelAddress = shopAddress sp,
         shopModelProducts = productModelList
-    }
-
-mapModelToCutomer :: CustomerModel -> Customer
-mapModelToCutomer customerModel = Customer {
-        customerId = customerModelId customerModel,
-        customerName = customerModelName customerModel,
-        customerAddress = customerModelAddress customerModel
-    }
-
-mapModelToShop :: ShopModel -> Shop
-mapModelToShop shopModel = Shop {
-        shopId = shopModelId shopModel,
-        shopName = shopModelName shopModel,
-        shopAddress = shopModelAddress shopModel
-    }
-
-mapModelToProduct :: ProductModel -> Product
-mapModelToProduct productModel =
-    let spId = shopModelId <$> productModelShop productModel
-    in Product {
-        productId = productModelId productModel,
-        productShopId = fromMaybe (-1) spId,
-        productName = productModelName productModel,
-        productPrice = productModelPrice productModel,
-        productColor = productModelColor productModel
-    }
-
-mapModelToOrder :: OrderModel -> Order
-mapModelToOrder orderModel =
-    let ordId = customerModelId <$> orderModelCustomer orderModel
-    in Order {
-            orderId = orderModelId orderModel,
-            orderCustomerId = fromMaybe (-1) ordId,
-            orderNumber = orderModelNumber orderModel
     }
